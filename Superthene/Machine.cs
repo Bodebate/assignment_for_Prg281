929af7b6c0f046c0eb17e3f34f6bd9a2cb7ad087
JustAiden25 <aidenmoses25@gmail.com>
2025-08-18 19:30:44 +0200

    Update Machine.cs

--- Superthene/Machine.cs
+++ Superthene/Machine.cs
@@ -11,8 +11,9 @@ namespace Superthene
     {
         private int _machineID;
         private string _machineDetails;
-
+         // Gets the machine Id used in the production process
         public int MachineID { get { return _machineID; } }
+        //Gets the details of the machine
         public string MachineDetails { get { return _machineDetails; } }
         // Constructor: Assigns a unique machine ID based on the machine list count.
         public Machine(IList<Machine> list,string machineDetails)
