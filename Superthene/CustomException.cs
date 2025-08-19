41d04d8280d1791a80c64c3253f232fd7b872cc8
JustAiden25 <aidenmoses25@gmail.com>
2025-08-19 15:15:10 +0200

    Update CustomException.cs

--- Superthene/CustomException.cs
+++ Superthene/CustomException.cs
@@ -7,7 +7,11 @@ using System.Threading.Tasks;
 namespace Superthene
 {
     // Custom exception class for the Superthene project (extend as needed for specific error handling).
-    internal class CustomException
+        internal class CustomException : Exception
     {
+        public CustomException(string message) : base(message)
+        {
+
+        }
     }
 }
